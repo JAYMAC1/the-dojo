4c579ffaefe75c4164fb6424941abb40f877af6e
James McNeil <mcneil_james@hotmail.com>
2022-01-15 19:34:36 +0000

    inital  firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDW7bgJ7g4jiSVfMdB2RpUwBtePTBDcqAI',
+  authDomain: 'thedojosite-c4964.firebaseapp.com',
+  projectId: 'thedojosite-c4964',
+  storageBucket: 'thedojosite-c4964.appspot.com',
+  messagingSenderId: '568756439756',
+  appId: '1:568756439756:web:1dc6501b1304f1762f1069',
+}
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+// init services
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+
+// timestamp
+const timestamp = firebase.firestore.Timestamp
+
+export { projectFirestore, projectAuth, timestamp }
