1da916c4e7a908745b48858e34b3e0a274c8a882
James McNeil <mcneil_james@hotmail.com>
2022-01-16 19:38:58 +0000

    imported Avatar component into Sidebar component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,15 +2,20 @@
 import './Sidebar.css'
 import DashboardIcon from '../assets/dashboard_icon.svg'
 import AddIcon from '../assets/add_icon.svg'
+
+// Pages, Components and Hooks
 import { NavLink } from 'react-router-dom'
+import { useAuthContext } from '../hooks/useAuthContext'
+import Avatar from './Avatar'
 
 const Sidebar = () => {
+  const { user } = useAuthContext()
   return (
     <div className='sidebar'>
       <div className='sidebar-content'>
         <div className='user'>
-          {/* avatar anf username here later */}
-          <p>Hey user,</p>
+          <Avatar src={user.photoURL} />
+          <p>Hey {user.displayName}</p>
         </div>
         <nav className='links'>
           <ul>
