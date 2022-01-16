a8f173f1a204c663244965d63a979d1442ffa1f8
James McNeil <mcneil_james@hotmail.com>
2022-01-16 14:51:36 +0000

    imported useSignup and added user to firebase and image to storage with signup function

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { useSignup } from '../../hooks/useSignup'
 
 // styles
 import './Signup.css'
@@ -9,6 +10,7 @@ const Signup = () => {
   const [displayName, setDisplayName] = useState('')
   const [thumbnail, setThumbnail] = useState(null)
   const [thumbnailError, setThumbnailError] = useState(null)
+  const { signup, error, isPending } = useSignup()
 
   const handleFileChange = (e) => {
     setThumbnail(null)
@@ -35,7 +37,7 @@ const Signup = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log(email, password, displayName, thumbnail)
+    signup(email, password, displayName, thumbnail)
   }
   return (
     <form onSubmit={handleSubmit} className='auth-form'>
@@ -72,7 +74,13 @@ const Signup = () => {
         <input type='file' required onChange={handleFileChange} />
         {thumbnailError && <div className='error'>{thumbnailError}</div>}
       </label>
-      <button className='btn'>Sign Up</button>
+      {isPending && (
+        <button className='btn' disabled>
+          Loading
+        </button>
+      )}
+      {!isPending && <button className='btn'>Sign Up</button>}
+      {error && <div className='error'>{error}</div>}
     </form>
   )
 }
