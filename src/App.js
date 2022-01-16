8f0ac045b66a4f53ec0b6053f8471d3cbb2eec25
James McNeil <mcneil_james@hotmail.com>
2022-01-16 09:16:34 +0000

    imported created page components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+// styles
 import './App.css'
+
+// Pages and Components
+import Dashboard from './pages/dashboard/Dashboard'
+import Login from './pages/login/Login'
+import Project from './pages/project/Project'
+import Signup from './pages/signup/Signup'
+import Create from './pages/create/Create'
 
 function App() {
   return (
-    <div className="App">
-
+    <div className='App'>
+      <BrowserRouter>
+        <div className='container'>
+          <Switch>
+            <Route exact path='/'>
+              <Dashboard />
+            </Route>
+            <Route path='/create'>
+              <Create />
+            </Route>
+            <Route path='/login'>
+              <Login />
+            </Route>
+            <Route path='/signup'>
+              <Signup />
+            </Route>
+            <Route path='/projects/:id'>
+              <Project />
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
 export default App
