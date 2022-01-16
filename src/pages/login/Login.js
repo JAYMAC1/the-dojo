c43fbdc32fbddb3e3a66a7ee929b16e392365636
James McNeil <mcneil_james@hotmail.com>
2022-01-16 17:42:49 +0000

    created login form

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,8 +1,48 @@
+import { useState } from 'react'
+import { useLogin } from '../../hooks/useLogin'
+
 // styles
 import './Login.css'
 
 const Login = () => {
-  return <div>Login</div>
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const { login, isPending, error } = useLogin()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    login(email, password)
+  }
+  return (
+    <form className='auth-form' onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      <label>
+        <span>email:</span>
+        <input
+          type='email'
+          required
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input
+          type='password'
+          required
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      {isPending && (
+        <button className='btn' disabled>
+          Logging In
+        </button>
+      )}
+      {!isPending && <button className='btn'>Login</button>}
+      {error && <div className='error'>{error}</div>}
+    </form>
+  )
 }
 
 export default Login
