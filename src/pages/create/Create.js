de739fbdb9a30fe5cf649f6e87475ee71292a287
James McNeil <mcneil_james@hotmail.com>
2022-01-17 20:34:27 +0000

    created projectobject

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -2,7 +2,8 @@ import { useEffect } from 'react'
 import { useState } from 'react'
 import Select from 'react-select'
 import { useCollection } from '../../hooks/useCollection'
-
+import { timestamp } from '../../firebase/config'
+import { useAuthContext } from '../../hooks/useAuthContext'
 // sytles
 import './Create.css'
 
@@ -17,12 +18,15 @@ const Create = () => {
   const { documents } = useCollection('users')
   const [users, setUsers] = useState([])
 
+  const { user } = useAuthContext()
+
   // form fields
   const [name, setName] = useState('')
   const [details, setDetails] = useState('')
   const [dueDate, setDueDate] = useState('')
   const [category, setCategory] = useState('')
   const [assignedUsers, setAssignedUsers] = useState([])
+  const [formError, setFormError] = useState(null)
 
   useEffect(() => {
     if (documents) {
@@ -38,7 +42,40 @@ const Create = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log(name, details, dueDate, category.value, assignedUsers)
+    setFormError(null)
+    if (!category) {
+      setFormError('Please select a project category')
+      return
+    }
+    if (assignedUsers.length < 1) {
+      setFormError('Please assign the project to at least 1 user.')
+      return
+    }
+
+    const createdBy = {
+      displayName: user.displayName,
+      photoURL: user.photoURL,
+      id: user.uid,
+    }
+
+    const assignedUsersList = assignedUsers.map((u) => {
+      return {
+        displayName: u.value.displayName,
+        photoURL: u.value.photoURL,
+        id: u.value.id,
+      }
+    })
+
+    const project = {
+      name,
+      details,
+      category: category.value,
+      dueDate: timestamp.fromDate(new Date(dueDate)),
+      comments: [],
+      createdBy,
+      assignedUsersList,
+    }
+    console.log(project)
   }
   return (
     <div className='create-form'>
@@ -87,6 +124,7 @@ const Create = () => {
           />
         </label>
         <button className='btn'>Add Project</button>
+        {formError && <p className='error'>{formError}</p>}
       </form>
     </div>
   )
