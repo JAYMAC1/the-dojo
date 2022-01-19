7b4da63b19ec45b407c57be46f5f82a5073f6054
James McNeil <mcneil_james@hotmail.com>
2022-01-19 21:50:06 +0000

    mapped through comments and display some info and added styles

--- src/pages/project/ProjectComments.js
+++ src/pages/project/ProjectComments.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Avatar from '../../components/Avatar'
 import { timestamp } from '../../firebase/config'
 import { useAuthContext } from '../../hooks/useAuthContext'
 import { useFirestore } from '../../hooks/useFirestore'
@@ -29,6 +30,25 @@ const ProjectComments = ({ project }) => {
   return (
     <div className='project-comments'>
       <h4>Project Comments</h4>
+
+      <ul>
+        {project.comments.length > 0 &&
+          project.comments.map((comment) => (
+            <li key={comment.id}>
+              <div className='comment-author'>
+                <Avatar src={comment.photoURL} />
+                <p>{comment.displayName}</p>
+              </div>
+              <div className='comment-date'>
+                <p>date here</p>
+              </div>
+              <div className='comment-content'>
+                <p>{comment.content}</p>
+              </div>
+            </li>
+          ))}
+      </ul>
+
       <form onSubmit={handleSubmit} className='add-comment'>
         <label>
           <span>Add new comment:</span>
