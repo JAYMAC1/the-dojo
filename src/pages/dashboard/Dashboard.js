a21141959ef143dfb1958f4d45f078cc0443fb8f
James McNeil <mcneil_james@hotmail.com>
2022-01-20 13:40:22 +0000

    Imported ProjectFilter into Dashboard

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -3,6 +3,7 @@ import { useCollection } from '../../hooks/useCollection'
 
 // styles
 import './Dashboard.css'
+import ProjectFilter from './ProjectFilter'
 
 const Dashboard = () => {
   const { documents, error } = useCollection('projects')
@@ -10,6 +11,7 @@ const Dashboard = () => {
     <div>
       <h2 className='page-title'>Dashboard</h2>
       {error && <p className='error'>{error}</p>}
+      {documents && <ProjectFilter />}
       {documents && <ProjectList projects={documents} />}
     </div>
   )
