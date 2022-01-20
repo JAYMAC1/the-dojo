a6f78b1ee45f93e30ee2778109385f7151e0a9db
James McNeil <mcneil_james@hotmail.com>
2022-01-20 13:51:15 +0000

    added state for filter

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import ProjectList from '../../components/ProjectList'
 import { useCollection } from '../../hooks/useCollection'
 
@@ -6,12 +7,23 @@ import './Dashboard.css'
 import ProjectFilter from './ProjectFilter'
 
 const Dashboard = () => {
+  const [currentFilter, setCurrentFilter] = useState('all')
   const { documents, error } = useCollection('projects')
+
+  const updateFilter = (filter) => {
+    setCurrentFilter(filter)
+  }
+
   return (
     <div>
       <h2 className='page-title'>Dashboard</h2>
       {error && <p className='error'>{error}</p>}
-      {documents && <ProjectFilter />}
+      {documents && (
+        <ProjectFilter
+          currentFilter={currentFilter}
+          updateFilter={updateFilter}
+        />
+      )}
       {documents && <ProjectList projects={documents} />}
     </div>
   )
